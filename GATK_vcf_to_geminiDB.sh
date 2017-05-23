72822e87ff2d030c12ac4ba9c0c5d9fe55967127
David McGaughey <mcgaughey@gmail.com>
2017-05-22 20:23:20 -0400

    changed temp dir to scratch, removed old commented out code

--- GATK_vcf_to_geminiDB.sh
+++ GATK_vcf_to_geminiDB.sh
@@ -9,36 +9,33 @@ VCF=$1
 ped=$2
 gemini_db_name=$3
 
-mkdir tmp
-
-#only keep AF > 0.25
-#vcffilter -f "AF > 0.25" $VCF > tmp/$VCF.AFfiltered
 
 #vt to "left align and trim alternative variants"
 cat $VCF \
 	| sed 's/ID=AD,Number=./ID=AD,Number=R/' \
 	| ~/git/vt/./vt decompose -s - \
 	| ~/git/vt/./vt normalize -r /data/mcgaugheyd/genomes/1000G_phase2_GRCh37/human_g1k_v37_decoy.fasta - \
-	> tmp/${VCF%.gz}
+	> /scratch/mcgaugheyd/${VCF%.gz}
 
 # annotate with VEP
-/home/mcgaugheyd/git/variant_prioritization/src/run_VEP.sh tmp/${VCF%.gz} GRCh37 $SLURM_CPUS_PER_TASK
+/home/mcgaugheyd/git/variant_prioritization/src/run_VEP.sh /scratch/mcgaugheyd/${VCF%.gz} GRCh37 $SLURM_CPUS_PER_TASK
+echo 'VEP done'
 
 # compress and index
-bgzip tmp/${VCF%.vcf.gz}.VEP.GRCh37.vcf
-tabix -p vcf tmp/${VCF%.vcf.gz}.VEP.GRCh37.vcf.gz
+bgzip /scratch/mcgaugheyd/${VCF%.vcf.gz}.VEP.GRCh37.vcf
+tabix -p vcf /scratch/mcgaugheyd/${VCF%.vcf.gz}.VEP.GRCh37.vcf.gz
+echo 'bgzip and tabix done'
 
 # annotate with custom annotations
 vcfanno -p $SLURM_CPUS_PER_TASK -lua /home/mcgaugheyd/git/variant_prioritization/src/vcfanno_custom.lua \
 	/home/mcgaugheyd/git/variant_prioritization/src/vcfanno_exomes.conf \
-	tmp/${VCF%.vcf.gz}.VEP.GRCh37.vcf.gz | bgzip > tmp/${VCF%.vcf.gz}.VEP.GRCh37.anno.vcf.gz
-tabix -p vcf tmp/${VCF%.vcf.gz}.VEP.GRCh37.anno.vcf.gz
+	/scratch/mcgaugheyd/${VCF%.vcf.gz}.VEP.GRCh37.vcf.gz | \
+	bgzip > /scratch/mcgaugheyd/${VCF%.vcf.gz}.VEP.GRCh37.anno.vcf.gz
+tabix -p vcf /scratch/mcgaugheyd/${VCF%.vcf.gz}.VEP.GRCh37.anno.vcf.gz
+echo 'vcfanno done'
 
 # move out of tmp folder
-mv tmp/${VCF%.vcf.gz}.VEP.GRCh37.anno.vcf.* . 
-
-# delete temp files
-rm -rf tmp
+mv /scratch/mcgaugheyd/${VCF%.vcf.gz}.VEP.GRCh37.anno.vcf.gz* . 
 
 # create gemini db
 vcf2db.py ${VCF%.vcf.gz}.VEP.GRCh37.anno.vcf.gz $ped $gemini_db_name
