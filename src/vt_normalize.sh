03980f279a8e2f6453253ad20ae35d0c69f8b8bf
David McGaughey <mcgaughey@gmail.com>
2018-01-05 15:19:18 -0500

    does ID type fix

--- src/vt_normalize.sh
+++ src/vt_normalize.sh
@@ -13,12 +13,20 @@ input=$2
 output=$3
 
 if [ "$genome" == "GRCh38" ]; then
-    vt normalize $2 -r /data/mcgaugheyd/genomes/GRCh38/hs38DH.fa -o $3
+    cat $input \
+		| sed 's/ID=AD,Number=./ID=AD,Number=R/' \
+		| ~/git/vt/./vt decompose -s - \
+		| ~/git/vt/./vt normalize -r /data/mcgaugheyd/genomes/GRCh38/hs38DH.fa - -o $3
 elif [ "$genome" == "hg19" ]; then
-    vt decompose -s $2 \
-	| vt normalize - -r /fdb/igenomes/Homo_sapiens/UCSC/hg19/Sequence/WholeGenomeFasta/genome.fa -o $3
+	cat $input \
+        | sed 's/ID=AD,Number=./ID=AD,Number=R/' \
+        | ~/git/vt/./vt decompose -s - \
+        | ~/git/vt/./vt normalize -r /fdb/igenomes/Homo_sapiens/UCSC/hg19/Sequence/WholeGenomeFasta/genome.fa - -o $3
 elif [ "$genome" == "1000g_b37" ]; then
-	vt normalize $2 -r /data/mcgaugheyd/genomes/1000g_phase2_v37/human_g1k_v37.fasta -o $3 
+	cat $input \
+        | sed 's/ID=AD,Number=./ID=AD,Number=R/' \
+		| ~/git/vt/./vt decompose -s - \
+        | ~/git/vt/./vt normalize -r /data/mcgaugheyd/genomes/1000G_phase2_GRCh37/human_g1k_v37_decoy.fasta - -o $3
 else
     echo "Pick either GRCh38, hg19, or 1000g_b37 reference genome"
 fi
