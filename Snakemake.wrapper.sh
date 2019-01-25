d8850609150e4e667283e5482c34d4e555acad95
David McGaughey <mcgaughey@gmail.com>
2019-01-25 16:56:11 -0500

    optional json

--- Snakemake.wrapper.sh
+++ Snakemake.wrapper.sh
@@ -14,11 +14,16 @@ sbcmd="sbatch --cpus-per-task={threads} \
 --error={cluster.error} \
 {cluster.extra}"
 
+if ["$2" -eq 0]; then
+	json="/home/$USER/git/variant_prioritization/src/cluster.json"
+else
+	json="$2"
+fi
 
 snakemake -s /home/$USER/git/variant_prioritization/src/Snakefile \
 -pr --local-cores 2 --jobs 1999 \
 --configfile $1 \
---cluster-config /home/$USER/git/variant_prioritization/src/cluster.json \
+--cluster-config $json \
 --cluster "$sbcmd"  --latency-wait 120 --rerun-incomplete \
 -k --restart-times 0 --resources res=1 
 
